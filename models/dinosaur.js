502f81310e691f3324bcb0c1f440964bd6ce7ed5
CameronTait <cameron-tait@hotmail.com>
2020-05-06 09:06:36 +0100

    first commit

---
+++ models/dinosaur.js
@@ -0,0 +1,7 @@
+const Dinosaur = function (species, diet, visitorsAttractedPerDay) {
+  this.species = species;
+  this.diet = diet;
+  this.visitorsAttractedPerDay = visitorsAttractedPerDay;
+}
+
+module.exports = Dinosaur;
---
+++ models/park.js
@@ -0,0 +1,8 @@
+const Park = function (species, diet, guestsAttractedPerDay) {
+    this.name = name;
+    this.ticketPrice = ticketPrice;
+    this.dinosaurs = [];
+  }
+  
+  module.exports = Dinosaur;
+  
